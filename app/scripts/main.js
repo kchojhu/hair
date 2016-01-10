adb1482a64a3dab27eed85d399059dc0f4eac44b
kchojhu@gmail.com <kchojhu@gmail.com>
2016-01-09 23:57:57 -0500

    what?

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,4 +2,8 @@
 console.log('\'Allo \'Allo!');
 $(function () {
     $.material.init();
+
+    $(window).on("load resize", function () {
+        $(".fill-screen").css("height", window.innerHeight);
+    });
 });
