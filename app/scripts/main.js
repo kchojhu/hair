7487ece15899bbff9bb4cb33b441b4ecda76e3b3
kchojhu@gmail.com <kchojhu@gmail.com>
2016-01-16 00:21:55 -0500

    no way

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,10 +7,10 @@ $(function () {
         $(".fill-screen").css("height", window.innerHeight);
     });
 
-    $("#video-wallpaper").background({
-        source: {
-            poster: "resource/img/Salon_Banner.jpg",
-            //video: "www.youtube.com/watch?v=IqTtV-LsTqY"
-        }
-    })
+//    $("#video-wallpaper").background({
+//        source: {
+//            poster: "../images/Salon_Banner.jpg",
+//            //video: "www.youtube.com/watch?v=IqTtV-LsTqY"
+//        }
+//    })
 });
